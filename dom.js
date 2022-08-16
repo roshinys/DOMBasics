22ab6c2e45f5828c9385412587d70fa84cbc3f3d
roshin Y S <roshinchaya@gmail.com>
2022-08-16 15:33:02 +0530

    basicDOMV1

---
+++ dom.js
@@ -0,0 +1,16 @@
+// console.dir(document)
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.head);
+// console.log(document.body);
+// const title = (document.title = "New Title");
+// console.log(title);
+// console.log(document.all[10]);
+// document.all[10].innerText = "Items";
+var headerTitle = document.getElementById("header-title");
+var header = document.getElementById("main-header");
+// headerTitle.innerHTML = `<h3>Items</h3>`;
+// headerTitle.innerText = "Items"
+header.style.borderBottom = "3px solid black";
+var addItem = document.getElementById("addItem");
+addItem.style.color = "green";
