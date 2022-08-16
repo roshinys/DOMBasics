fc2ff5d40ae8a4f46f9edc183c326e20b7dcf551
roshin Y S <roshinchaya@gmail.com>
2022-08-16 15:52:09 +0530

    getByTag V3

--- dom.js
+++ dom.js
@@ -7,17 +7,22 @@
 // console.log(title);
 // console.log(document.all[10]);
 // document.all[10].innerText = "Items";
-var headerTitle = document.getElementById("header-title");
-var header = document.getElementById("main-header");
+// var headerTitle = document.getElementById("header-title");
+// var header = document.getElementById("main-header");
 // headerTitle.innerHTML = `<h3>Items</h3>`;
 // headerTitle.innerText = "Items"
-header.style.borderBottom = "3px solid black";
-var addItem = document.getElementById("addItem");
-addItem.style.color = "green";
-var items = document.getElementsByClassName("list-group-item");
+// header.style.borderBottom = "3px solid black";
+// var addItem = document.getElementById("addItem");
+// addItem.style.color = "green";
+// var items = document.getElementsByClassName("list-group-item");
 // console.log(items);
-const thirdElement = items[2];
-thirdElement.style.backgroundColor = "green";
-for (let i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "Bold";
+// const thirdElement = items[2];
+// thirdElement.style.backgroundColor = "green";
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = "Bold";
+// }
+var li = document.getElementsByTagName("li");
+console.log(li);
+for (let i = 0; i < li.length; i++) {
+  li[i].style.fontWeight = "Bold";
 }
